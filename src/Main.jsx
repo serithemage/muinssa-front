ab66daab743395f85f425c845c8876d43dcaef6c
jakemraz <jakemraz100@gmail.com>
2021-02-15 23:43:07 +0900

    skeleton

---
+++ src/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Page404 from './components/Page404';
+
+export default function Main() {
+  return (
+    <div>
+      <Switch>
+        <Route
+          exact path='/'
+          render={() => <HomePage />}
+        />
+        <Route
+          exact path='/rooms'
+          render={() => {/*<RoomListPage />*/}}
+        />
+        <Route
+          exact path='/rooms/:id'
+          render={() => {/*<Room />*/}}
+        />
+        <Route component={Page404} />
+      </Switch>
+		</div>
+	)
+}
---
+++ src/components/Page404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page404 = () => (
+  <div>
+    404, Page not found
+  </div>
+);
+
+export default Page404;
