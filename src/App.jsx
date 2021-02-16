bb8b924a7e923a242836c7c50da8f51692442a0c
jakemraz <jakemraz100@gmail.com>
2021-02-16 17:10:38 +0900

    add appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore } from 'redux';
-
+import { AppBar, Toolbar } from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 import Main from './Main';
 import { createContext } from 'react';
+import { Typography } from '@material-ui/core';
 
 export const userContext = createContext({
   nickname: ''
 });
 
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+    alignItems: 'center'
+  },
+}));
+
 function App() {
-  return (
+
+  const classes = useStyles();
+
+  return ( 
     <Router>
+      <AppBar position="static" className={classes.title}>
+        <Toolbar>
+        <Typography variant="h6">
+          MuInSsa : I'm Not InSsa
+        </Typography>
+        </Toolbar>
+      </AppBar>
       <div className="App">
         <Main />
       </div>
+      
     </Router>
   );
 }
