4f0e8f36402242e2fe1df40bca458a524ecd50d3
jakemraz <jakemraz100@gmail.com>
2021-02-16 20:45:47 +0900

    room: add exit button

--- src/components/RoomPage.jsx
+++ src/components/RoomPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {TextField,
   Button,
   List,
@@ -9,12 +10,28 @@ import {TextField,
   Container} from '@material-ui/core';
 
 
-export default function RoomPage(props) {
+const useStyles = makeStyles((theme) => ({
+  exit: {
+    margin: theme.spacing(3,0,3)
+  }
+}));  
 
+export default function RoomPage(props) {
+  const classes = useStyles();
   const [attendees, setAttendees] = useState([]);
 
   return (
     <Container maxWidth="xs">
+      <Button
+        className={classes.exit}
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        margin="normal"
+      >
+        Exit
+      </Button>
       RoomPage {props.match.params.id}
     </Container>
   );
